11edd491a18370bb7f7f7bbe2b8cf3caf3c71ee4
Johannes Skøien <johannes_skoien@hotmail.com>
2022-02-09 22:07:44 +0100

    Matrix

--- matrix.js
+++ matrix.js
@@ -78,3 +78,49 @@ canvas.width = cw;
 canvas.height = ch;
 
 let frames = 0;
+
+class FallingChar {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    draw(context) {
+        this.value =
+            charArray[
+                Math.floor(Math.random() * (charArray.length - 1))
+            ].toUpperCase();
+
+        this.speed = (Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
+
+        context.fillStyle = "rgba(0, 255, 0)";
+        context.font = fontSize + "px sans-serif";
+        context.fillText(this.value, this.x, this.y);
+        this.y += this.speed;
+        if (this.y > ch) {
+            this.y = (Math.random() * ch) / 2 - 50;
+            this.x = Math.floor(Math.random() * maxColumns) * fontSize;
+            this.spee =
+                (-Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
+        }
+    }
+}
+
+let update = () => {
+    if (fallingArray.length < maxCharacters) {
+        let fallingChar = new FallingChar(
+            Math.floor(Math.random() * maxColumns) * fontSize,
+            (Math.random() * ch) / 2 - 50
+        );
+        fallingArray.push(fallingChar);
+    }
+    context.fillStyle = "rgba(0,0,0,0.05)";
+    context.fillRect(0, 0, cw, ch);
+    for (let i = 0; i < fallingArray.length && frames % 2 == 0; i++) {
+        fallingArray[i].draw(context);
+    }
+    requestAnimationFrame(update);
+    frames++;
+};
+
+update();
