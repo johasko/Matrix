3ee6d9289bff954f33bebcb8b3bb55f2eaf3f413
Johannes Skøien <johannes_skoien@hotmail.com>
2022-02-09 21:24:28 +0100

    Setup array

---
+++ matrix.js
@@ -0,0 +1,80 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+let cw = window.innerWidth;
+let ch = window.innerHeight;
+
+let charArray = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+];
+
+let maxCharacters = 1000;
+let fallingArray = [];
+let fontSize = 15;
+let maxColumns = cw / fontSize;
+
+canvas.width = cw;
+canvas.height = ch;
+
+let frames = 0;
